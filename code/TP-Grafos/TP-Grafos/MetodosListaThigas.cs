613b7ebf7b27a0ea41a75121337b8c1a1fb50faf
Thiago Caetano Dantas <127804127+VirtuousThigas@users.noreply.github.com>
2024-12-07 00:09:28 -0300

    Questão 5,6,7,8

--- code/TP-Grafos/TP-Grafos/MetodosListaThigas.cs
+++ code/TP-Grafos/TP-Grafos/MetodosListaThigas.cs
@@ -24,12 +24,48 @@ public int[] ImprimirVerticesAdjacente(int v)
    return dados[v].ToArray();
 }
 
-public int[] ImprimirVerticesIncidentes(int v)
+public int [] ImprimirArestasIncidentes(int v, int w)
 {
-    if(v<=0 || v >= Lenght)
+    int[] arestasIncidentes = new int[Lenght];
+    if(w>=0 && w< Lenght)
     {
-        return new int[0];
+        for(int i = 0 ; i< Lenght;i++)
+        {
+            if(dados[w,i]!=0 && dados[w,i] != dados[v,w])
+            {
+                arestasIncidentes.Append(dados[w,i]);
+            }
+
+        }
+        return arestasIncidentes;
     }
-   return dados[v].ToArray();
 }
-public
+
+// public int[] ImprimirVerticesIncidentes(int v)
+// {
+//     if(v<=0 || v >= Lenght)
+//     {
+//         return new int[0];
+//     }
+//    return dados[v].ToArray();
+// }
+
+public int ImprimirGrau(int v)
+{
+    if(v==0 || v>Lenght)
+    {
+        return 0;
+    }
+    return dados[v].Count;
+}
+public bool VerificarAdjacencia(int v, int w)
+{
+    if(v == 0 || w==0 || v> Lenght|| w>Lenght)
+    {
+        return false;
+    }
+    return dados[v].Contains(w)|| dados[w].Contains(v);  
+}
+public void SubstituirPeso(Aresta a)
+{
+}
